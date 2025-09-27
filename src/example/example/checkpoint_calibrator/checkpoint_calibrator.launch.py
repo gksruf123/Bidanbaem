c8cc5c30ad26ca6852b0071097ed2d3ce911671a
gksruf1 <duagksruf1@gmail.com>
2025-09-27 18:03:31 +0900

    chore: modify another another issue

--- src/example/example/checkpoint_calibrator/checkpoint_calibrator.launch.py
+++ src/example/example/checkpoint_calibrator/checkpoint_calibrator.launch.py
@@ -42,7 +42,7 @@ def launch_setup(context):
         parameters=[{
             'rgb_topic': '/ascamera/camera_publisher/rgb0/image',
             'depth_topic': '/ascamera/camera_publisher/depth0/image_raw',
-            'classes': ['crosswalk', 'go', 'green', 'red'],
+            'classes': ['go', 'right', 'park', 'red', 'green', 'crosswalk'],
             'hfov_deg': 69.0,
             'samples': 30,
         }]
