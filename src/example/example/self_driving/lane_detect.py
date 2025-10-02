02213df1024e833f65a34d7f6a2568904598b0ec
hyeyun Hwang <hhyeyoun@naver.com>
2025-10-02 18:58:45 +0900

    chore : 경로수정

--- src/example/example/self_driving/lane_detect.py
+++ src/example/example/self_driving/lane_detect.py
@@ -27,7 +27,8 @@ config_path = os.path.join(base_path, "lab_config.yaml")       # 최상위 Bidan
 
 lab_data = common.get_yaml_data(config_path)
 """
-lab_data = common.get_yaml_data("/home/ubuntu/software/lab_tool/lab_config.yaml")
+#lab_data = common.get_yaml_data("/home/ubuntu/software/lab_tool/lab_config.yaml")
+lab_data = common.get_yaml_data("/shared/Bidanbaem/lab_config.yaml")
 
 class LaneDetector(object):
     def __init__(self, color):
