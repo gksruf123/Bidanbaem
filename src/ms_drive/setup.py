1b1dc42de5536cc9657e3f217c5bde27f8a89c4b
Minsung <pauaup@naver.com>
2025-10-05 16:58:17 +0900

    build: add data_files to setup.py

--- src/ms_drive/setup.py
+++ src/ms_drive/setup.py
@@ -10,6 +10,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/slam.launch.py']),  # <- important
+        ('share/' + package_name + '/config', [
+            'config/slam_params.yaml',
+            'config/ekf.yaml',
+            'config/nav_params.yaml'
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
