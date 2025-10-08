b6393912bb3a37957f8d4104fc98099b423769fd
gksruf1 <duagksruf1@gmail.com>
2025-10-08 22:06:15 +0900

    test: test a new value

--- src/example/example/self_driving/self_driving.py
+++ src/example/example/self_driving/self_driving.py
@@ -554,7 +554,7 @@ class SelfDrivingNode(Node):
                         d_est = d_min if self.dmin_ema is None else (1 - alpha) * self.dmin_ema + alpha * d_min
                         self.dmin_ema = d_est
 
-                        NEAR, FAR = 0.40, 0.55
+                        NEAR, FAR = 0.30, 0.55
                         if d_est < FAR:
                             strength = (FAR - d_est) / max(FAR - NEAR, 1e-6)
                             strength = float(np.clip(strength, 0.0, 1.0))
