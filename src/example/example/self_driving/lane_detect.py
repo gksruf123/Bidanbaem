8cc44305c65a48114d50e1c0df443eae9ec70a8a
hyeyun Hwang <hhyeyoun@naver.com>
2025-10-09 16:40:15 +0900

    fix: update lab_config.yaml path detection and adjust ROI for ascamera

--- src/example/example/self_driving/lane_detect.py
+++ src/example/example/self_driving/lane_detect.py
@@ -15,23 +15,22 @@ from cv_bridge import CvBridge
 bridge = CvBridge()
 
 username = os.getenv("USER")
-if username == "pi":
-    config_path = "/home/ubuntu/software/lab_tool/lab_config.yaml"
+
+# 사용자 환경에 맞춰 자동 경로 선택
+if username in ("pi", "ubuntu"):
+    if os.path.exists("/home/ubuntu/Bidanbaem/lab_config.yaml"):
+        config_path = "/home/ubuntu/Bidanbaem/lab_config.yaml"
+    elif os.path.exists("/home/ubuntu/software/lab_tool/lab_config.yaml"):
+        config_path = "/home/ubuntu/software/lab_tool/lab_config.yaml"
+    else:
+        config_path = "/shared/Bidanbaem/lab_config.yaml"
 else:
     config_path = "/home/intel/Bidanbaem/lab_config.yaml"
+
+
+# YAML 파일 불러오기
 lab_data = common.get_yaml_data(config_path)
 
-
-
-"""
-current_dir = os.path.dirname(os.path.abspath(__file__))       # 현재 파일 경로
-base_path = os.path.abspath(os.path.join(current_dir, "../../../../.."))  
-config_path = os.path.join(base_path, "lab_config.yaml")       # 최상위 Bidanbaem 경로에서 lab_config.yaml 찾기
-
-lab_data = common.get_yaml_data(config_path)
-"""
-#lab_data = common.get_yaml_data("/home/ubuntu/software/lab_tool/lab_config.yaml")
-#lab_data = common.get_yaml_data("/shared/Bidanbaem/lab_config.yaml")
 
 class LaneDetector(object):
     def __init__(self, color):
